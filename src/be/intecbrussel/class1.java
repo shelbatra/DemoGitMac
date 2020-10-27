441e2222a1db9f0733b366c9f297016d389b2f7b
shelbatra <shelbatra.jashari@gmail.com>
2020-10-27 22:17:41 +0100

    test

---
+++ src/be/intecbrussel/class1.java
@@ -0,0 +1,5 @@
+package be.intecbrussel;
+
+public class class1 {
+
+}
---
+++ src/be/intecbrussel/class2.java
@@ -0,0 +1,8 @@
+package be.intecbrussel;
+
+public class class2 {
+
+    public static void main(String[] args) {
+        int number = 1;
+    }
+}
